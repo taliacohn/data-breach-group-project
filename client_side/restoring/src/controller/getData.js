44e73f3bc096a8abf0885eb908a3d38122afbfa1
Kirill Shiryaev <infantem.help@gmail.com>
2023-01-04 10:28:47 +0200

    separate server connection

---
+++ client_side/restoring/src/controller/getData.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+function getData(url, handleError, handleThen) {
+  axios.get(SERVER_URL + url, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).catch((err) => {
+    if (err) {
+      handleError(err);
+    }
+  }).then((response) => { handleThen(response) });
+};
+
+function putData(url, data, handleError, handleThen) {
+  axios.put(SERVER_URL + url, data).catch(
+    (err) => {
+      if (err) {
+        handleError(err);
+      }
+    }
+  ).then((response) => {
+    handleThen(response);
+  }
+  );
+};
+export { getData, putData };
